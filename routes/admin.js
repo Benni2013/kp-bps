00bb4f05717b46c772b4cbf49c297197c070187c
MuhammadRaihanAlghifari <alghifari2096@gmail.com>
2025-01-20 10:13:15 +0700

    manajemen ang ang

--- routes/admin.js
+++ routes/admin.js
@@ -7,7 +7,23 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/manajemen_anggota', function(req, res, next) {
-  res.render('admin/manajemen_anggota', { title: 'Express' });
+  res.render('admin/manajemenAnggota/manajemen_anggota', { title: 'Express' });
+});
+
+router.get('/tambah_anggota', function(req, res, next) {
+  res.render('admin/manajemenAnggota/tambah_anggota', { title: 'Express' });
+});
+
+router.get('/detail_anggota', function(req, res, next) {
+  res.render('admin/manajemenAnggota/detail_anggota', { title: 'Express' });
+});
+
+router.get('/edit_anggota', function(req, res, next) {
+  res.render('admin/manajemenAnggota/edit_anggota', { title: 'Express' });
+});
+
+router.get('/ubahpw_anggota', function(req, res, next) {
+  res.render('admin/manajemenAnggota/ubahpw_anggota', { title: 'Express' });
 });
 
 router.get('/manajemen_kriteria', function(req, res, next) {
