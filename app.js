d5e4c19570cd92c4d507016f1c86d11373b2e0a3
LENOVO <frizqyadelaa@gmail.com>
2025-01-21 13:39:41 +0700

    baru commit

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var adminsRouter = require('./routes/admin');
+var supervisorsRouter = require('./routes/supervisor');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin', adminsRouter);
+app.use('/supervisor', supervisorsRouter);
 
 
 // catch 404 and forward to error handler
