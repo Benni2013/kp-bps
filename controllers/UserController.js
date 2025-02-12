fa83678746fd22421a61a942525d2714c0d20f56
MuhammadRaihanAlghifari <alghifari2096@gmail.com>
2025-02-12 10:17:45 +0700

    last

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -7,7 +7,7 @@
 
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const { Op, where } = require("sequelize");
+const { Op, where, json } = require("sequelize");
 const { Anggota, Pemilihan, DetailPemilihan, DataNilai, Voting1, Voting2, Indikator, Periode } = require("../models");
 require("dotenv").config();
 
@@ -96,35 +96,153 @@ const getDataPemilihan = async (req, res, next) => {
   }
 };
 
+// const getPegawaiTerbaik = async (req, res, next) => {
+//   try {
+//     let role = req.cookies.role;
+//     const akun = req.user;
+//     let pemilihan = await Pemilihan.findAll({
+//       where: {
+//         tahap_pemilihan: "selesai",
+//       },
+//     });
+//     if (!pemilihan) {
+//       res.render("user/beranda", {
+//         title: "Beranda",
+//         layout: "layouts/layout",
+//         role,
+//         akun,
+//       });
+//     } else {
+//       let pegawaiTerbaik = true;
+//       res.render("user/beranda", {
+//         title: "Beranda",
+//         pegawaiTerbaik: pegawaiTerbaik,
+//         layout: "layouts/layout",
+//         role,
+//         akun,
+//       });
+//     }
+//   } catch (error) {
+//     console.error("getPegawaiTerbaik error:", error);
+//     res.redirect("/users/beranda");
+//   }
+// };
+
 const getPegawaiTerbaik = async (req, res, next) => {
   try {
-    let role = req.cookies.role;
-    const akun = req.user;
-    let pemilihan = await Pemilihan.findAll({
+    const activePemilihan = await Pemilihan.findOne({
       where: {
-        tahap_pemilihan: "selesai",
+        tahap_pemilihan: {
+          [Op.ne]: "tutup",
+        },
       },
+      include: [
+        {
+          model: Periode,
+          attributes: ["nama_periode"],
+        },
+      ],
+      order: [["tanggal_mulai", "DESC"]],
     });
-    if (!pemilihan) {
-      res.render("user/beranda", {
-        title: "Beranda",
-        layout: "layouts/layout",
-        role,
-        akun,
-      });
-    } else {
-      let pegawaiTerbaik = true;
-      res.render("user/beranda", {
-        title: "Beranda",
-        pegawaiTerbaik: pegawaiTerbaik,
-        layout: "layouts/layout",
-        role,
-        akun,
-      });
+
+    // Get pemilihan data dengan periode
+
+    if (!activePemilihan) {
+      return next(eror);
     }
+
+    const pemilihanTitle = `${activePemilihan.nama_pemilihan} ${activePemilihan.Periode.nama_periode} Tahun ${activePemilihan.tahun}`;
+
+    // Get jumlah indikator aktif
+    const jumlahIndikator = await Indikator.count({
+      where: { status_inditakor: "aktif" },
+    });
+
+    // Get jumlah pengisi penilaian
+    const jumlahPengisi = await DetailPemilihan.count({
+      where: { pemilihan_id: activePemilihan.pemilihan_id },
+      include: [
+        {
+          model: Voting2,
+          required: true,
+          attributes: [],
+        },
+      ],
+      distinct: true,
+    });
+
+    // Get semua kandidat yang lolos
+    const kandidat = await DetailPemilihan.findAll({
+      where: {
+        pemilihan_id: activePemilihan.pemilihan_id,
+        "$Voting1.status_anggota$": "lolos",
+      },
+      include: [
+        {
+          model: Anggota,
+          attributes: ["nip", "nama", "jabatan", "gender", "foto"],
+        },
+        {
+          model: Voting1,
+          required: true,
+          attributes: ["status_anggota"],
+        },
+      ],
+      group: ["DetailPemilihan.detail_pemilihan_id", "Anggotum.nip", "Anggotum.nama", "Anggotum.jabatan", "Anggotum.gender", "Anggotum.foto", "Voting1.status_anggota"],
+    });
+
+    console.log(JSON.stringify(kandidat,null,2))
+
+    // Get nilai semua kandidat
+    const hasilKriteria = await Promise.all(
+      kandidat.map(async (k) => {
+        const totalPoin = await Voting2.sum("nilai", {
+          where: { kandidat_id: k.anggota_id },
+        });
+
+        // Hitung rata-rata
+        const rataRata = (totalPoin / jumlahPengisi / (jumlahIndikator * 4)) * 100;
+
+        let foto_default = k.Anggotum.gender === "pria" ? "/default_pp/lk.png" : "/default_pp/pr.png";
+
+        return {
+          nama: k.Anggotum.nama,
+          nip: k.Anggotum.nip,
+          jabatan: k.Anggotum.jabatan,
+          foto: k.Anggotum.foto || foto_default,
+          totalPoin,
+          rataRata: parseFloat(rataRata.toFixed(2)),
+        };
+      })
+    );
+
+    // Sort berdasarkan rata-rata tertinggi
+    hasilKriteria.sort((a, b) => b.rataRata - a.rataRata);
+
+    // Ambil kandidat dengan nilai tertinggi
+    const pemenang = hasilKriteria[0];
+
+    if (!pemenang) {
+      return res.status(404).send("Pemenang tidak ditemukan");
+    }
+
+    // console.log('\nData Pemenang:');
+    // console.log(JSON.stringify(pemenang, null, 2));
+
+    // Update status pemilihan menjadi selesai
+    await Pemilihan.update({ tahap_pemilihan: "selesai" }, { where: { pemilihan_id: activePemilihan.pemilihan_id } });
+
+    res.render("user/beranda", {
+      title: "Beranda",
+      pegawaiTerbaik: pemenang,
+      layout: "layouts/layout",
+      pemilihanTitle,
+      pemenang,
+      akun: req.user,
+    });
   } catch (error) {
-    console.error("getPegawaiTerbaik error:", error);
-    res.redirect("/users/beranda");
+    console.error("Error getting pegawai terbaik:", error);
+    next(error);
   }
 };
 
@@ -206,18 +324,19 @@ const getRiwayat = async (req, res, next) => {
                   // Tambahkan skor ke kandidat
                   kandidatKriteria[n].skor += voting2.nilai;
 
-                    console.log("zzzzzzzzzzzz" + kandidatKriteria[n].skor);
-                    console.log("++++++++++++" + voting2.kandidat_id);
+                  console.log("zzzzzzzzzzzz" + kandidatKriteria[n].skor);
+                  console.log("++++++++++++" + voting2.kandidat_id);
                 }
               }
             }
           }
-        pemilihan[k].skor_pemenang = kandidatKriteria[n].skor;
-
+          pemilihan[k].skor_pemenang = kandidatKriteria[n].skor;
+          console.log(">>>>>>>>>>>>>>>>>>>> " + pemilihan[k].skor_pemenang);
         }
         let topScorer = kandidatKriteria.reduce((highest, kandidat) => {
           return kandidat.skor > highest.skor ? kandidat : highest;
         });
+        console.log("<<<<<<<<<<<<<<<<<<< " + topScorer.skor);
         let idPemenang = await DetailPemilihan.findOne({
           where: {
             detail_pemilihan_id: topScorer.detail_pemilihan_id,
@@ -230,8 +349,9 @@ const getRiwayat = async (req, res, next) => {
           },
         });
         pemilihan[k].nama_pemenang = namaPemenang.nama;
+        pemilihan[k].divisi = namaPemenang.divisi;
 
-        console.log("Nama Pemenang:", namaPemenang.nama);
+        console.log("Nama Pemenang:", pemilihan[k].nama_pemenang);
       }
       console.log("//////////////////////" + final.length + "///" + pemilihan.length + "///" + anggota.length + "///" + indikator.length);
 
